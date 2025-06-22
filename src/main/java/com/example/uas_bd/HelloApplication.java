49a6f24ee1b83480f0ce2d03bcbc43e20a91f22c
Jericho1024 <jerichosundjaja@gmail.com>
2025-06-22 18:47:20 +0700

    git pull origin main

--- src/main/java/com/example/uas_bd/HelloApplication.java
+++ src/main/java/com/example/uas_bd/HelloApplication.java
@@ -10,11 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
-        System.out.println("sean");
         stage.show();
     }
 
